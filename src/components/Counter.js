2280278747e525f4377cf61718be7b360023f178
Steven Fisher <steven.fisher22@gmail.com>
2019-03-12 11:44:45 -0500

    functional counter app with redux and state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class Counter extends React.Component {
-    state = { count: 0 }
 
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
+        this.props.dispatch({type: "INCREMENT"})
     }
 
     decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        })
+        this.props.dispatch({type: "DECREMENT"})
+    }
+
+    reset = () => {
+        this.props.dispatch({type: "RESET"})
     }
 
     render() {
@@ -21,12 +21,19 @@ class Counter extends React.Component {
                 <h2>Counter</h2>
                 <div>
                     <button onClick={this.decrement}>-</button>
-                    <span className="count">{this.state.count}</span>
+                    <span className="count">{this.props.count}</span>
                     <button onClick={this.increment}>+</button>
+                    <button onClick={this.reset}>Reset</button>
                 </div>
             </div>
         )
     }
 }
 
-export default Counter
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    }
+}
+
+export default connect(mapStateToProps)(Counter)
--- src/index.js
+++ src/index.js
@@ -2,14 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Components
 import Counter from './components/Counter';
 
-function Test() {
+// Redux
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+    count: 0
+}
+
+function reducer(state = initialState, action) {
+    console.log('reducer', state, action);
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET': 
+            return {
+                count: 0
+            };
+        default: 
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'RESET' });
+
+const App = () => {
     return (
-        <>
+        <Provider store={store}>
             <Counter />
-        </>
+        </Provider>
     )
 }
 
-ReactDOM.render(<Test />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
