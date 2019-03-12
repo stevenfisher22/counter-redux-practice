493e5854a394125588aad26a279cabaad2429a2f
Steven Fisher <steven.fisher22@gmail.com>
2019-03-12 10:57:21 -0500

    counter setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Counter from './components/Counter';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Test() {
+    return (
+        <>
+            <Counter />
+        </>
+    )
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'))
